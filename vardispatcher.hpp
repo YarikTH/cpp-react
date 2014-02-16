866c88a847c31d5dcfc28998e3b7490891a1f0b9
Eduard Knyshov <edvorg@gmail.com>
2014-02-16 23:42:12 +0400

    vardispatcher implemented

--- vardispatcher.hpp
+++ vardispatcher.hpp
@@ -3,6 +3,10 @@
 #ifndef VARDISPATCHER_HPP
 #define VARDISPATCHER_HPP
 
+#include <list>
+#include <unordered_map>
+#include "varlistener.hpp"
+
 namespace react {
 
     template <class T>
@@ -11,22 +15,37 @@ namespace react {
     template <class T>
     class VarDispatcher {
     public:
+        using Var = Var<T>;
+        using Listeners = std::list<VarListener *>;
+        using VarsListeners = std::unordered_map<const Var *, Listeners>;
+
         static auto & instance();
 
-        void connect(const Var<T> & var) {
-            // TODO implement this
+        void connect(const Var & var) {
+            varsListeners[&var] = {};
         }
 
-        void disconnect(const Var<T> & var) {
-            // TODO implement this
+        void disconnect(const Var & var) {
+            varsListeners.erase(&var);
         }
 
-        void notifyChange(const Var<T> & var) {
-            // TODO implement this
+        void notifyChange(const Var & var) {
+            auto listeners = varsListeners.find(&var);
+
+            if (listeners != varsListeners.end()) {
+                for (auto & listener : listeners->second) {
+                    listener->update();
+                }
+            }
+            else {
+                // error, we are not owninng this var
+            }
         }
 
     private:
         VarDispatcher() = default;
+
+        VarsListeners varsListeners;
     };
 
     template <class T>
