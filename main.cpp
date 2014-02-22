ac726507e470668bd8d7e4138d79c57721bae67a
Eduard Knyshov <edvorg@gmail.com>
2014-02-23 03:02:21 +0400

    random tests

--- main.cpp
+++ main.cpp
@@ -1,39 +1,56 @@
 #include <iostream>
+#include <stdlib.h>
 #include <react.hpp>
 
 using namespace std;
 using namespace react;
 
 #define TEST(x) cout<<"testing: " #x <<((x)?": OK":": FAIL")<<endl;
+#define TEST_PASSES 10
+#define TEST_RAND_LIMIT 10
 
 int main() {
-    cout << "begin testing" << endl;
+    srand(time(nullptr));
 
-    auto var0 = var(4);
-    auto var1 = var0 * var0;
-    auto var2 = var0 + var1;
+    cout << "begin testing\n" << endl;
+
+    auto var01 = var(4);
+    auto var02 = var(1);
+    auto var1 = var01 * var02;
+    auto var2 = var01 + var1;
     auto var3 = var1 + var2;
     auto var4 = var3 == var3;
 
     auto test = [&] {
-        cout << "var0: " << var0() << endl;
+        cout << "var01: " << var01() << endl;
         cout << "var1: " << var1() << endl;
         cout << "var2: " << var2() << endl;
         cout << "var3: " << var3() << endl;
         cout << "var4: " << var4() << endl;
 
-        TEST(var1() == var0() * var0());
-        TEST(var2() == var0() + var1());
+        TEST(var1() == var01() * var02());
+        TEST(var2() == var01() + var1());
         TEST(var3() == var1() + var2());
         TEST(var4() == (var3() == var3()));
+
+        cout << endl;
     };
 
     test();
 
-    var0 = 2;
-    test();
+    for (unsigned int i = 0; i < TEST_PASSES; ++i) {
+        if (rand() % 2) {
+            cout << "var01 changed" << endl;
+            var01 = rand() % TEST_RAND_LIMIT;
+        }
+        if (rand() % 2) {
+            cout << "var02 changed" << endl;
+            var02 = rand() % TEST_RAND_LIMIT;
+        }
+        test();
+    }
 
-    cout << "end testing" << endl;
+    cout << "end testing\n" << endl;
 
     return 0;
 }
