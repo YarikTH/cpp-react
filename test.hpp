754c158fc670414566041fae26f0f0bfd827eda4
Eduard Knyshov <edvorg@gmail.com>
2014-02-24 01:37:39 +0400

    + attempts counting for test logger

--- test.hpp
+++ test.hpp
@@ -7,6 +7,12 @@ namespace test {
 
     class Log {
     public:
+        class Info {
+        public:
+            int failes = 0;
+            int attempts = 0;
+        };
+
         static void log(const std::string & str, int fail) {
             auto resstr = std::string(fail ? ": FAIL" : ": OK");
             auto it = failes.find(str);
@@ -14,24 +20,26 @@ namespace test {
             std::cout << "testing: " << str << resstr << std::endl;
 
             if (it != failes.end()) {
-                it->second += fail;
+                it->second.failes += fail;
+                it->second.attempts++;
             }
             else {
-                failes[str] = fail;
+                failes[str] = { fail, 1 };
             }
         }
 
         static void stats() {
             for (auto & it : failes) {
-                std::cout << it.first << ": " << it.second << " fail(es)" << std::endl;
+                std::cout << it.first << ": " << it.second.failes << " failes ";
+                std::cout << it.second.attempts << " attempts"<< std::endl;
             }
         }
 
     private:
-        static std::unordered_map<std::string, int> failes;
+        static std::unordered_map<std::string, Info> failes;
     };
 
-    std::unordered_map<std::string, int> Log::failes;
+    std::unordered_map<std::string, Log::Info> Log::failes;
 
 }
 
